e43f98638e7ce8d708bab15b7f2d47fabc217a90
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-22 17:08:25 +0530

    Add files via upload

--- JS_Files/19.JS-SMS-App/app.js
+++ JS_Files/19.JS-SMS-App/app.js
@@ -1,1 +1,8 @@
-
+//SMS App
+let textAreaEl = document.querySelector('#text-area');
+let charCountEl =document.querySelector('#char-count');
+textAreaEl.addEventListener('keyup',function () {
+    let textLength
+    textLength = textAreaEl.value.length;
+    charCountEl.innerText = textLength;
+});
