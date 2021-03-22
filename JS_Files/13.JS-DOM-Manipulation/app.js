656f33f416b606fcc64bac84574601eb36fa3e9c
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-22 16:52:59 +0530

    Add files via upload

--- JS_Files/13.JS-DOM-Manipulation/app.js
+++ JS_Files/13.JS-DOM-Manipulation/app.js
@@ -1,1 +1,38 @@
 
+console.log(document);
+
+console.log(document.title);
+//change title
+document.title = 'UI techno';
+
+console.log(document.head);
+console.log(document.body);
+
+// Accessing the nav tag
+let navTag = document.querySelector('nav');
+console.log(navTag);
+
+// Anchor tag
+let anchorTag = document.querySelector('nav a');
+console.log(anchorTag);
+
+//innerText
+let theText = anchorTag.innerText;
+console.log(theText);
+
+// change the inner text
+anchorTag.innerText = 'Neelam Rana';
+
+
+//h1 tag
+let h1Tag = document.querySelector('#msg');
+h1Tag.innerText = 'Good Evening';
+console.log(h1Tag);
+
+// style
+h1Tag.style.backgroundColor = 'lightsalmon';
+h1Tag.style.textAlign = 'center';
+h1Tag.style.padding = '20px';
+h1Tag.style.padding = '20px';
+h1Tag.style.boxShadow = '0 0 10px black';
+
