d935475c0edd6071ce4d016a77b5f8387b42e441
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-22 16:25:17 +0530

    Add files via upload

---
+++ JS_Files/02.JS-Variable-Declarations/App.js
@@ -0,0 +1,9 @@
+ let a=50 ,b=50;
+let c= a+b;
+console.log('sum of  a, b is :',c)//ES5
+console.log('sum of '+ a+', '+ b+' is : '+ c);//ES5
+console.log(`sum of ${a}, ${b} is : ${c}`);//ES6
+
+let employeeMarks=3.5
+console.log(`employee Marks is: ${employeeMarks}, ${typeof employeeMarks}`)
+console.log(typeof employeeMarks)
