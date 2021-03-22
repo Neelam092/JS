5fc2482473f13b064955562ceba7e9f51adfd3ad
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-22 16:59:49 +0530

    Add files via upload

--- JS_Files/15.JS-Keyup-events/app.js
+++ JS_Files/15.JS-Keyup-events/app.js
@@ -1,1 +1,8 @@
-
+// keyup Event example
+let  textBox = document.querySelector('#username');
+textBox.addEventListener('keyup',function(){
+    //alert('entered a text')
+    let textEntered = textBox.value;
+    let messageElement = document.querySelector('#msg');
+    messageElement.innerText = textEntered;
+});
