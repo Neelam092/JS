1b317ae058436dc8868c18433390cf1745d64810
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-22 16:49:39 +0530

    Add files via upload

--- JS_Files/11.JS-Num-Str-JSON-Obj/app.js
+++ JS_Files/11.JS-Num-Str-JSON-Obj/app.js
@@ -1,1 +1,65 @@
+// Number Objects
 
+// min number
+console.log(Number.MIN_SAFE_INTEGER);
+
+// max number
+console.log(Number.MAX_SAFE_INTEGER);
+
+// positive infinity
+console.log(Number.POSITIVE_INFINITY);
+
+// negative infinity
+console.log(Number.NEGATIVE_INFINITY);
+
+// Parse Int
+let str = '100.78';
+let num = Number.parseInt(str);
+console.log(`${typeof str} => ${typeof num}`);
+
+
+// toString
+let num1 = 100;
+let numString = num1.toString();
+console.log(`${typeof num1} => ${typeof numString}`);
+
+// toFixed
+let amount = 520;
+console.log(amount.toFixed(2));
+
+// ------ String Related Operations -----------
+let message = String('Good Morning');
+// length
+console.log(message.length);
+// lowercase
+console.log(message.toLowerCase());
+// uppercase
+console.log(message.toUpperCase());
+//subString(start index ,  no of chars
+console.log(message.substr(3,8));
+// charAt
+console.log(message.charAt(3));
+// charCodeAt(Ascii)
+console.log(message.charCodeAt(3));
+
+// JSON Object
+/*
+       string => object: parse()
+       object => string: stringify()
+ */
+
+let student = {
+    name : 'Raj',
+    age : 40,
+    course : 'CSE',
+    college : 'IIT Mumbai'
+};
+console.log(student);
+
+// object to string
+let studentStr = JSON.stringify(student);
+console.log(studentStr);
+
+// string to object
+let newStudent =JSON.parse(studentStr);
+console.log(newStudent);
