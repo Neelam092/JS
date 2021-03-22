d4f485704265ecf068a75ec60bd23bac43ad78c7
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-22 17:27:34 +0530

    Add files via upload

--- JS_Files/26.JS-Scopes/app.js
+++ JS_Files/26.JS-Scopes/app.js
@@ -1,1 +1,15 @@
+// JS Scopes
+let a =10;  //  global / parent
+{
+    let a = 20; // local/child
+}
+    console.log(a);
 
+// function
+let greet = function () {
+    let msg;
+    msg = 'Good Morning';
+    return msg ;
+}
+let result = greet();
+console.log(result);
