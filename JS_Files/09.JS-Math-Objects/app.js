c163e304ca737a7f7a784731680abfa48f120e45
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-22 16:42:36 +0530

    Add files via upload

--- JS_Files/09.JS-Math-Objects/app.js
+++ JS_Files/09.JS-Math-Objects/app.js
@@ -1,1 +1,23 @@
+//sqrt
+let sqrt =Math.sqrt(144);
+console.log(`sqrt of 144 is : ${sqrt}`);
 
+//pow
+let pow = Math.pow(2,5);
+console.log(`2 ^ 5 is : ${pow}`);
+
+//min
+let min = Math.min(12,54,6,7,43,555,3333,222,122);
+console.log(min);
+
+//max
+let max = Math.max(12,54,6,7,43,555,3333,222,122);
+console.log(max);
+
+//random
+let random = Math.random();
+console.log(random);
+
+//random ,round
+let rand =Math.round(Math.random()*100000);
+console.log(rand);
