64888f79210f244bbc628e833d8b5706f5a59460
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-22 17:06:42 +0530

    Add files via upload

--- JS_Files/18.JS-DOM-Range-Select/app.js
+++ JS_Files/18.JS-DOM-Range-Select/app.js
@@ -1,1 +1,15 @@
+// Range Select
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
+// format number
+let formatNumber = (number) =>{
+let result = new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(number);
+    return result;
+};
 
+let rangeEl = document.querySelector('#range');
+let amountEl = document.querySelector('#amount');
+rangeEl.addEventListener('input',function () {
+    let selectedAmount = rangeEl.value;
+    amountEl.innerText =formatNumber(selectedAmount);
+
+})
