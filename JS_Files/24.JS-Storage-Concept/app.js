5da2a7e86345e59e5f76216440a7a119ba096974
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-22 17:24:16 +0530

    Add files via upload

--- JS_Files/24.JS-Storage-Concept/app.js
+++ JS_Files/24.JS-Storage-Concept/app.js
@@ -1,1 +1,41 @@
+// Local Strorage for simple string
+
+let empName = 'John';
+
+// Set
+localStorage.setItem('name',empName);
+
+// Get
+console.log(localStorage.getItem('name'));
+
+// Delete the Key
+localStorage.removeItem('name');
+
+// clear
+localStorage.clear();
+
+// Local Storage with an array
+let colors = [{ id: 1, name :'Blue'},
+              { id: 2, name :'Green'},
+              { id: 3, name :'Yellow'},
+              { id: 4, name :'Pink'}];
+localStorage.setItem('colors',JSON.stringify(colors));
+console.log( localStorage.getItem('colors'));
+
+let theColors = JSON.parse(localStorage.getItem('colors'));
+console.log(theColors);
+
+// Storage With simple string
+let studentName = 'Rajan';
+
+// Set
+sessionStorage.setItem('name',studentName);
+
+// Get
+console.log(sessionStorage.getItem('name'));
+
+// Delete
+sessionStorage.removeItem('name');
+// clear
+sessionStorage.clear();
 
