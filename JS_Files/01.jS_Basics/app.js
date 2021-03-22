b719400ff75e5aff6dcc633d33bb48f4f21a4603
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-22 16:21:42 +0530

    Update app.js

--- JS_Files/01.jS_Basics/app.js
+++ JS_Files/01.jS_Basics/app.js
@@ -1,4 +1,4 @@
 console.log('Good Morning');
-console.info('This is a informatin message ');
+console.info('This is a information message ');
 console.error('This is a error message');
 console.warn('This is warning message');
