5a0752daa920721a3d95bb9716013a46c8855744
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-22 17:12:54 +0530

    Add files via upload

--- JS_Files/21.JS-Math-table/app.js
+++ JS_Files/21.JS-Math-table/app.js
@@ -1,1 +1,26 @@
+// Math Table
+let numberEl = document.querySelector('#req-number');
+numberEl.addEventListener('keyup',function () {
+    let theNumber;
+    theNumber = Number(numberEl.value);
+    document.querySelector('#number').inneeText = theNumber;
+    calculate();
+});
 
+let rangeEl = document.querySelector('#multiplier');
+rangeEl.addEventListener('input',function () {
+    let selectedMultiplier;
+    selectedMultiplier = Number(rangeEl.value);
+    document.querySelector('#multi').innerText = selectedMultiplier;
+    calculate();
+});
+
+// Calculate
+let calculate= () => {
+    let theNumber = Number(document.querySelector('#number').innerText);
+    let multiplier;
+    multiplier = Number(document.querySelector('#multi').innerText);
+    let MathResult;
+    MathResult = theNumber * multiplier;
+    document.querySelector('#result').innerText = MathResult;
+};
