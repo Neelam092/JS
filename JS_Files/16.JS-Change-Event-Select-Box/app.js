7f01f462e3174a9c6b90674a50ffffbae741225d
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-22 17:03:43 +0530

    Add files via upload

--- JS_Files/16.JS-Change-Event-Select-Box/app.js
+++ JS_Files/16.JS-Change-Event-Select-Box/app.js
@@ -1,1 +1,8 @@
-
+//change event on select box
+let selectBox = document.querySelector('#lang-select');
+selectBox.addEventListener('change',function(){
+  //  alert('selected an option');
+    let selectedOption = selectBox.value;
+    let languageOptionEl = document.querySelector('#lang-option');
+    languageOptionEl.innerText = selectedOption;
+})
