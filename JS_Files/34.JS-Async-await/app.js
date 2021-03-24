8c98f01d37e0961c0ceb584a79a4a0a2045a59e2
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-24 07:54:27 +0530

    Add files via upload

--- JS_Files/34.JS-Async-await/app.js
+++ JS_Files/34.JS-Async-await/app.js
@@ -1,1 +1,36 @@
-
+let employees =[{id : 1, name : 'John', age:40 },{id : 2, name : 'Doe', age  :34 }];
+// create Employee
+let createEmployee  = (employee) =>{
+    return new Promise((resolve,reject) =>{
+        setTimeout(()=>{
+            employees.push(employee);
+            let isDone = true;
+            if(isDone){
+                resolve();
+            }
+            else{
+                reject('Employee Creation is Failed');
+            }
+        },2000);
+    });
+};
+// get employee
+let getEmployees = () => {
+    setTimeout( () => {
+        let employeeRows = '';
+        employees.forEach((employee) =>{
+            employeeRows += `<tr>
+                            <td>${employee.id}</td>
+                            <td>${employee.name}</td>
+                            <td>${employee.age}</td>
+                         </tr>`;
+        });
+        document.querySelector('#table-body').innerHTML = employeeRows;
+    },1000);
+};
+// async  , await
+let init = async ()=> {
+    let result = await createEmployee({id:3, name:'Wilson', age:30});
+    getEmployees();
+}
+init();
