78a6de05768a4a3f979ef8de1444824e26aed430
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-24 07:45:04 +0530

    Add files via upload

--- JS_Files/28.JS-Classes/app.js
+++ JS_Files/28.JS-Classes/app.js
@@ -1,1 +1,44 @@
+/*let mobile = {
+    color : 'Silver',
+    price : 35000,
+    brand : 'Apple'
+};
+console.log(mobile);*/
+
+class Mobile{
+    constructor(color, price,brand){
+        this.color = color;
+        this.price = price;
+        this.brand = brand;
+    }
+    getColor(){
+        return this.color;
+    }
+    setColor(color){
+        this.color = color;
+    }
+    getPrice() {
+        return this.price;
+    }
+    setPrice(price){
+        this.price = price;
+        }
+    getBrand(){
+        return this.brand;
+    }
+    setBrand(brand){
+        this.brand = brand;
+    }
+    printSpecification(){         //  printSpecification() instance method to fetch data
+        let spec = `COLOR : ${this.getColor()}
+                    PRICE : ${this.getPrice()}
+                    BRAND : ${this.getBrand()}`;
+        console.log(spec);
+    }
+}
+
+let mobile = new Mobile('Silver',35000,'Apple');
+mobile.printSpecification();
+mobile.setPrice(45000);
+mobile.printSpecification();
 
