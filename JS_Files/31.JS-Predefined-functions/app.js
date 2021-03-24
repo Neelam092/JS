b5362e383318ab0fa4bbfd1b69cc8ff9f74e7031
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-24 07:50:44 +0530

    Add files via upload

--- JS_Files/31.JS-Predefined-functions/app.js
+++ JS_Files/31.JS-Predefined-functions/app.js
@@ -1,1 +1,14 @@
+let count = 0;
+let interval = 0;
+let greet = ()=> {
+    count++;
+    console.log(` Hello Welcome to JS : ${count}`);
+};
+interval = setInterval(greet,1000); // keep calling for every seconds
+
+console.log(new Date().toLocaleTimeString());
+setTimeout(() =>{
+    clearInterval(interval);
+},5000);
+
 
