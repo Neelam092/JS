21f01b2bea737a8a12bf523e6ed4ed0dd64ae62a
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-24 07:46:27 +0530

    Add files via upload

--- JS_Files/29.JS-Subclasses/app.js
+++ JS_Files/29.JS-Subclasses/app.js
@@ -1,1 +1,58 @@
+class Person{
+    constructor(firstName ,lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    getFirstName(){
+        return this.firstName;
+    }
+    getLastName(){
+        return this.lastName;
+    }
+    greet() {
+        let msg = `Welcome Mr : ${this.getFirstName()} ${this.getLastName()}`;
 
+        console.log(msg);
+    }
+}
+class Employee extends Person{
+    constructor(firstName , lastName, age, designation){
+        super(firstName, lastName);
+        this.age = age;
+        this.designation = designation;
+    }
+    getAge(){
+        return this.age;
+    }
+    getDesignation(){
+        return this.designation;
+    }
+    greet() {
+        let msg = `Hello Mr : ${this.getFirstName()} ${this.getLastName()} ${this.getAge()} ${this.getDesignation()}`;
+
+        console.log(msg);
+    }
+}
+let employee = new Employee('Jhon','Doe',40,'Manager');
+employee.greet();
+
+class Customer extends Person{
+    constructor(firstName,lastName,age, location) {
+        super(firstName,lastName);
+        this.age = age;
+        this.location = location;
+    }
+    getAge(){
+        return this.age;
+    }
+    getLocation(){
+        return this.location;
+    }
+    greet() {
+        let msg = `Hello Mr : ${this.getFirstName()} ${this.getLastName()} ${this.getAge()} ${this.getLocation()}`;
+
+        console.log(msg);
+    }
+}
+let customer = new Customer('Loura','Wilson',50,'USA')
+customer.greet();
